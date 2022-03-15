402cd0ce31ed432c2bc660ddfae2712ccaaa916e
Todd Pangilinan <pang.todd@gmail.com>
2022-03-14 23:22:23 -0700

    updated scripts.js and index.html, now the content of the different headings is hidden, and you can click on it to learn more.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#aboutMe").click(function() {
+    $("#about").toggle();
+  });
+
+  $("#meExperience").click(function() {
+    $("#experience").toggle();
+  });
+
+  $("#meProjects").click(function() {
+    $("#project").toggle();
+  });
+ 
+});
